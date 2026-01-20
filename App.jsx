986e81cdb2dd3fb48cc5aac0f34d8e98042e86ed
kaif0910 <mdkaifperwez@gmail.com>
2026-01-20 22:52:41 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import {Text, View,Image, Pressable, Alert} from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+      <Image
+        source={{uri: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_1280.jpg'}}
+        style={{width: 50, height: 50}} 
+      />
+
+      <Pressable style ={{padding: 10, backgroundColor: 'yellow'}} onPress={() => Alert.alert('Pressed!')}>
+        <Text>Press Me</Text>
+      </Pressable>  
+    </SafeAreaView>
+  );
+};
+
+export default App;
